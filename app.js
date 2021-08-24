b0e2934bdf61199de5d0bdfe5022b716fa5d1587
Almuhandis <abubakaralmuhandis@gmail.com>
2021-08-24 07:40:29 -0700

    game file update

--- app.js
+++ app.js
@@ -9,19 +9,7 @@ GAME RULES:
 
 */
 var roundScore, scores, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
-
-
-
-
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     // do something here
@@ -69,8 +57,29 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     // //next player
     //  nextPlayer();
 });
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
 
 
+
+};
 function nextPlayer(){
  // next player
  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
